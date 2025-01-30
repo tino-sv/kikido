1b8da4e6e4811fc7d0cbcf2b775cf703ee210c22
tinodev <tinovalentine5@gmail.com>
2025-01-30 02:16:24 -0500

    fixed deploy error 4

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const publishableKey = "pk_test_d2VsY29tZS1zd2lmdC0yNy5jbGVyay5hY2NvdW50cy5kZXYk";
 
 if (!publishableKey) {
   throw new Error("NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY is not set in the environment variables.");
