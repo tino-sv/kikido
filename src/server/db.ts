d9e3f70b41465985de6b07109e0154ae248f5427
tinodev <tinovalentine5@gmail.com>
2025-02-17 02:54:11 -0500

    fixed deploy errors

--- src/server/db.ts
+++ src/server/db.ts
@@ -7,7 +7,12 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
+    datasources: {
+      db: {
+        url: "file:./dev.db",
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
